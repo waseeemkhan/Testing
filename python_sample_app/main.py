203a1b4a45988c5a25e5015b53d3719ca956f3e6
waseeemkh@gmail.com <waseeemkh@gmail.com>
2021-08-12 09:59:02 -0400

    this is the second commit to see what we have

--- python_sample_app/main.py
+++ python_sample_app/main.py
@@ -6,3 +6,11 @@ z=5
 print(x,y,z)
 print("***************")
 print(float(3),int(3),str(3))
+print("*************")
+var=["waseem",68,2.6]
+x1,x2,x3=var
+print(x1)
+print(x2)
+print(x3)  
+print(x1,x2,"x3")
+print(var)
