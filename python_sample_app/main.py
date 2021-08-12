b7eb2ebfe9dd8d5648a442fded84765dc3fc4f8a
waseeemkh@gmail.com <waseeemkh@gmail.com>
2021-08-12 13:01:39 -0400

    this is my test commit

--- python_sample_app/main.py
+++ python_sample_app/main.py
@@ -1,6 +1,6 @@
 
 print ("Hello World!")
-x=3
+x=6
 y=4
 z=5
 print(x,y,z)
@@ -14,3 +14,8 @@ print(x2)
 print(x3)  
 print(x1,x2,"x3")
 print(var)
+#this comment signified the creation of the second branch
+if x>y:
+    print("my name is waseem khan")
+else:
+    print("well that didnt work")
